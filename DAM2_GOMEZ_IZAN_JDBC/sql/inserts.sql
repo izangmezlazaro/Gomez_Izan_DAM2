dc5693122f95fdb25a364aca195f67c0918ee6ee
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2026-06-01 10:15:24 +0200

    SQL

---
+++ DAM2_GOMEZ_IZAN_JDBC/sql/inserts.sql
@@ -0,0 +1,17 @@
+INSERT INTO laboratorio VALUES(1, 'akari lab', 'España', 'IZAN_GOMEZ');
+INSERT INTO laboratorio VALUES(2, 'origen biotech', 'Francia', 'IZAN_GOMEZ');
+INSERT INTO laboratorio VALUES(3, 'izanlabs', 'España', 'IZAN_GOMEZ');
+INSERT INTO laboratorio VALUES(4, 'karimlabs', 'España', 'IZAN_GOMEZ');
+INSERT INTO laboratorio VALUES(5, 'cristianolabs', 'España', 'IZAN_GOMEZ');
+
+INSERT INTO muestrasADN VALUES(1, 'adn1', 'orina', '2024-01-10', 1, 'IZAN_GOMEZ');
+INSERT INTO muestrasADN VALUES(2, 'adn2', 'sangre', '2022-02-15', 2, 'IZAN_GOMEZ');
+INSERT INTO muestrasADN VALUES(3, 'adn3', 'orina', '2021-03-08', 3, 'IZAN_GOMEZ');
+INSERT INTO muestrasADN VALUES(4, 'adn4', 'sangre', '2025-04-20', 4, 'IZAN_GOMEZ');
+INSERT INTO muestrasADN VALUES(5, 'adn5', 'sangre', '2025-05-12', 5, 'IZAN_GOMEZ');
+
+INSERT INTO detalle_muestraADN VALUES(1, 'SI', 98, 'Cancer prostata', 1, 'IZAN_GOMEZ');
+INSERT INTO detalle_muestraADN VALUES(2, 'NO', 85, 'Anemia general', 2, 'IZAN_GOMEZ');
+INSERT INTO detalle_muestraADN VALUES(3, 'SI', 92, 'Contenedor VIH', 3, 'IZAN_GOMEZ');
+INSERT INTO detalle_muestraADN VALUES(4, 'NO', 77, 'Flojo en vitamina', 4, 'IZAN_GOMEZ');
+INSERT INTO detalle_muestraADN VALUES(5, 'SI', 99, 'Perfecto estado', 5, 'IZAN_GOMEZ');
